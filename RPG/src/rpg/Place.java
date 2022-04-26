3fcc7e7411741929b3bfed9d9c818ca775f42c0a
KyleNicholls19 <104453756+KyleNicholls19@users.noreply.github.com>
2022-04-26 11:10:08 -0600

    Add files via upload

---
+++ RPG/src/rpg/Place.java
@@ -0,0 +1,14 @@
+package rpg;
+
+public class Place {
+	
+	private String place;
+	public Place(String place)
+	{   // System.out.println("AHAHAAH");
+		this.place = place;
+	}
+	public String getPlace()
+	{
+		return place;
+	}
+}
