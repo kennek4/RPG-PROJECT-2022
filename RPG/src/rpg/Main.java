81f2510af2e114ae6bed0c6a2296de2e36fe8f02
KyleNicholls19 <104453756+KyleNicholls19@users.noreply.github.com>
2022-05-04 10:44:47 -0600

    Add files via upload

--- RPG/src/rpg/Main.java
+++ RPG/src/rpg/Main.java
@@ -1,11 +1,16 @@
 package rpg;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
 
-class Main{
+
+
+public class Main implements KeyListener {
 
 	public static void main (String[] args) {
 		int count =0;
@@ -49,20 +54,21 @@ class Main{
 	        int count = 0;
 	        int choice = 0;
 	 
-	        ArrayList<Integer> options = new ArrayList<Integer>();
+	        HashMap<Integer,Graph.Node> options = new HashMap<>();
 	        while (true)
 	        {
 	        System.out.print("You are at: " + src_vertex +" "+ graph.adj_list.get(src_vertex).get(choice).place.getPlace());
 	        
 	            //traverse through the adjacency list and print the edges
-	            for (Graph.Node edge : graph.adj_list.get(src_vertex)) {
-	            	options.add(edge.id);	               
-	                                
-	            }
+	          	for(int i = 0; i<graph.adj_list.get(src_vertex).size(); i++ )
+	          	{
+	          		options.put(i+1, graph.adj_list.get(src_vertex).get(i));
+	          	}
+	          		
 	            System.out.print(" Your movement options are: " + options);
 	            System.out.print("\nWhere do you want to move?");
-	            choice = in.nextInt() - 1; 
-	            src_vertex = graph.adj_list.get(count).get(choice).id;
+	            choice = in.nextInt(); 
+	            src_vertex = graph.adj_list.get(choice).get(choice).id;
 	            System.out.println();
 	           
 	            options.clear();
@@ -70,5 +76,23 @@ class Main{
 	        }
 	
 
-	 }	
+	 }
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		char key = e.getKeyChar();
+		if (key == KeyEvent.VK_1) {
+			
+		}
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}	
 }
