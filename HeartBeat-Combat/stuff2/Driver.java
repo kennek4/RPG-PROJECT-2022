04f8259eadf2f1598e8e2b79d72e17581ac3158e
kennek4 <xktabz@gmail.com>
2022-05-12 23:16:00 -0600

    new ui system

--- HeartBeat-Combat/stuff2/Driver.java
+++ HeartBeat-Combat/stuff2/Driver.java
@@ -1,7 +1,16 @@
 package stuff2;
 
+import java.util.Scanner;
+
 public class Driver {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         GUI ui = new GUI();
+
+        while (true) {
+            System.out.print("\nReduce health by: ");
+            int dmg = sc.nextInt();
+            ui.healthBar.setValue(ui.healthBar.getValue() - dmg);
+        }
     }
 }
--- HeartBeat-Combat/stuff2/GUI.java
+++ HeartBeat-Combat/stuff2/GUI.java
@@ -5,17 +5,39 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.util.jar.JarEntry;
+import java.awt.GridLayout;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.border.TitledBorder;
 
 public class GUI {
 
+    // LEFT SIDE TOP HALF VARIABLES
     JFrame window;
+    JPanel playerInfoBox;
+    JPanel pTopHalfPanel;
+    JPanel playerPortrait;
+    JLabel gunName;
+    JPanel gunIMG;
+    JProgressBar healthBar;
+
+    // LEFT SIDE BOTTOM HALF VARIABLES
+    JPanel pBotHalfPanel;
+    JPanel abilityBox1;
+    JPanel abilityBox2;
+    JPanel abilityBox3;
+    JPanel abilityBox4;
+    JPanel abilityBox5;
+    JPanel abilityBox6;
+
+    // RIGHT SIDE
+    JPanel environmentInfoBox;
+
+    TitledBorder tBorder;
+    GridLayout grid;
 
     public GUI() {
         window = new JFrame("Test");
@@ -31,7 +53,7 @@ public class GUI {
         /**
          * Panel for the Left Side of the GUI
          */
-        JPanel playerInfoBox = new JPanel();
+        playerInfoBox = new JPanel();
         playerInfoBox.setLayout(new GridBagLayout());
         playerInfoBox.setBackground(Color.BLUE);
         playerInfoBox.setPreferredSize(new Dimension(500, 800));
@@ -41,10 +63,10 @@ public class GUI {
         window.add(playerInfoBox, c);
 
         // Left side top half
-        JPanel pTopHalfPanel = new JPanel();
+        pTopHalfPanel = new JPanel();
         pTopHalfPanel.setLayout(new GridBagLayout());
-        pTopHalfPanel.setPreferredSize(new Dimension(475, 350));
-        c.insets = new Insets(0, 10, 0, 10);
+        pTopHalfPanel.setPreferredSize(new Dimension(475, 300));
+        c.insets = new Insets(0, 5, 0, 5);
         c.gridx = 0;
         c.gridy = 0;
         c.gridwidth = 3;
@@ -52,30 +74,98 @@ public class GUI {
         playerInfoBox.add(pTopHalfPanel, c);
 
         // Player portrait - left side top half
-        JPanel playerPortrait = new JPanel();
+        playerPortrait = new JPanel();
         playerPortrait.setPreferredSize(new Dimension(200, 200));
         playerPortrait.setBackground(Color.LIGHT_GRAY);
         c.gridx = 0;
         c.gridy = 0;
         c.gridheight = 2;
         c.gridwidth = 2;
-        c.insets = new Insets(-5, 0, 0, 30);
-
+        c.insets = new Insets(5, 0, 10, 20);
         pTopHalfPanel.add(playerPortrait, c);
 
-        JLabel playerName = new JLabel("INSERT NAME HERE");
-        playerName.setVerticalAlignment(1);
-        playerName.setPreferredSize(new Dimension(150, 100));
+        gunName = new JLabel("AK-47");
+        gunName.setVerticalAlignment(JLabel.BOTTOM);
+        gunName.setHorizontalAlignment(JLabel.CENTER);
+        gunName.setPreferredSize(new Dimension(150, 20));
         c.gridx = 2;
         c.gridy = 0;
         c.gridheight = 1;
         c.gridwidth = 1;
-        pTopHalfPanel.add(playerName, c);
+        c.insets = new Insets(0, 0, 0, 0);
+
+        pTopHalfPanel.add(gunName, c);
+
+        gunIMG = new JPanel();
+        gunIMG.setBackground(Color.DARK_GRAY);
+        gunIMG.setPreferredSize(new Dimension(125, 150));
+        c.gridx = 2;
+        c.gridy = 1;
+        c.gridheight = 1;
+        c.gridwidth = 1;
+        pTopHalfPanel.add(gunIMG, c);
+
+        healthBar = new JProgressBar();
+        healthBar.setMaximum(200);
+        healthBar.setValue(200);
+        healthBar.setForeground(Color.GREEN);
+        healthBar.setBackground(Color.RED);
+        healthBar.setPreferredSize(new Dimension(healthBar.getWidth() + 15, 25));
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridheight = 1;
+        c.gridwidth = 3;
+        pTopHalfPanel.add(healthBar, c);
+
+        // Left side bottom half
+        pBotHalfPanel = new JPanel();
+        pBotHalfPanel.setBackground(Color.WHITE);
+        pBotHalfPanel.setLayout(new GridLayout(0, 2, 10, 10));
+        pBotHalfPanel.setPreferredSize(new Dimension(475, 450));
+        c.insets = new Insets(10, 5, 0, 5);
+        c.gridx = 0;
+        c.gridy = 2;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        playerInfoBox.add(pBotHalfPanel, c);
+
+        /**
+         * INITIALIZING ALL THE ABILITY BOXES
+         */
+        abilityBox1 = new JPanel();
+        abilityBox1.setBackground(Color.CYAN);
+        abilityBox1.setPreferredSize(new Dimension(100, 50));
+        pBotHalfPanel.add(abilityBox1);
+
+        abilityBox2 = new JPanel();
+        abilityBox2.setBackground(Color.CYAN);
+        abilityBox2.setPreferredSize(new Dimension(100, 50));
+        pBotHalfPanel.add(abilityBox2);
+
+        abilityBox3 = new JPanel();
+        abilityBox3.setBackground(Color.CYAN);
+        abilityBox3.setPreferredSize(new Dimension(100, 50));
+        pBotHalfPanel.add(abilityBox3);
+
+        abilityBox4 = new JPanel();
+        abilityBox4.setBackground(Color.CYAN);
+        abilityBox4.setPreferredSize(new Dimension(100, 50));
+        pBotHalfPanel.add(abilityBox4);
+
+        abilityBox5 = new JPanel();
+        abilityBox5.setBackground(Color.CYAN);
+        abilityBox5.setPreferredSize(new Dimension(100, 50));
+        pBotHalfPanel.add(abilityBox5);
+
+        abilityBox6 = new JPanel();
+        abilityBox6.setBackground(Color.CYAN);
+        abilityBox6.setPreferredSize(new Dimension(100, 50));
+        pBotHalfPanel.add(abilityBox6);
 
         /**
          * Panel for the Right Side of the GUI
          */
-        JPanel environmentInfoBox = new JPanel();
+        environmentInfoBox = new JPanel();
         environmentInfoBox.setBackground(Color.BLACK);
         environmentInfoBox.setPreferredSize(new Dimension(1000, 800));
         c.gridx = 1;
